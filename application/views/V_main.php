9676089c0a15bc7c760a1c7aac4e5cd82894d241
francobko <franco.condor.urp@gmail.com>
2019-10-27 01:37:18 +0300

    main - question

--- application/views/V_main.php
+++ application/views/V_main.php
@@ -11,49 +11,48 @@
     <link rel="stylesheet" href="<?php echo base_url(); ?>public/utils/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?php echo base_url(); ?>public/utils/css/style.css">
     <!-- Scrollbar Custom CSS -->
-    <link rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
-    
+
 </head>
-    <body>
-        <div class="container fd-column ai-baseline">
+
+<body>
+    <div class="container fd-column ai-baseline">
         <h1 class="text h1 mb-124px">Hi, Abel</h1>
         <div class="content flex-inherit">
-        <h3 class="text h3" id="question">¿How do you feel today?</h3>
-        <div class="list-boxes">
-            <div class="box-img" onclick="next(1)">
-            <img id="img1" src="<?php echo base_url(); ?>public/utils/img/medusin.png" alt="logo" >
+            <h3 class="text h3" id="question">¿How do you feel today?</h3>
+            <div class="list-boxes">
+                <div class="box-img" onclick="next(1)">
+                    <img id="img1" src="<?php echo base_url(); ?>public/utils/img/medusin.png" alt="logo">
+                </div>
+                <div class="box-img" onclick="next(2)">
+                    <img id="img2" src="<?php echo base_url(); ?>public/utils/img/medusin.png" alt="logo">
+                </div>
             </div>
-            <div class="box-img" onclick="next(2)">
-            <img id="img2" src="<?php echo base_url(); ?>public/utils/img/medusin.png" alt="logo" >
-            </div>
-        </div>
         </div>
     </div>
-        <script src="<?php echo base_url();?>public/utils/js/jquery-3.3.1.min.js"></script>
-        <script src="<?php echo base_url(); ?>public/utils/js/bootstrap.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
-        <script
-            src="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.concat.min.js"></script>
-        <script>
-            var array = [];
-            function next(option){
-                array.push(option);
-                if(array.length == 1){
-                    $("#question").text('¿Pregunta 2?');
-                    $("#img1").attr("src","<?php echo base_url(); ?>public/utils/img/instagram.png");
-                    $("#img2").attr("src","<?php echo base_url(); ?>public/utils/img/instagram.png");
-                } else if(array.length == 2){
-                    $("#question").text('¿Pregunta 3?');
-                    $("#img1").attr("src","<?php echo base_url(); ?>public/utils/img/youtube.png");
-                    $("#img2").attr("src","<?php echo base_url(); ?>public/utils/img/youtube.png");
-                } else if(array.length == 3){
-                    window.open("<?php echo base_url(); ?>" + "newmascota", "_self");
-                }
+    <script src="<?php echo base_url(); ?>public/utils/js/jquery-3.3.1.min.js"></script>
+    <script src="<?php echo base_url(); ?>public/utils/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.concat.min.js"></script>
+    <script>
+        var array = [];
+
+        function next(option) {
+            array.push(option);
+            if (array.length == 1) {
+                $("#question").text('¿Pregunta 2?');
+                $("#img1").attr("src", "<?php echo base_url(); ?>public/utils/img/instagram.png");
+                $("#img2").attr("src", "<?php echo base_url(); ?>public/utils/img/instagram.png");
+            } else if (array.length == 2) {
+                $("#question").text('¿Pregunta 3?');
+                $("#img1").attr("src", "<?php echo base_url(); ?>public/utils/img/youtube.png");
+                $("#img2").attr("src", "<?php echo base_url(); ?>public/utils/img/youtube.png");
+            } else if (array.length == 3) {
+                window.open("<?php echo base_url(); ?>" + "newmascota", "_self");
             }
-            
-        </script>
-    </body>
+        }
+    </script>
+</body>
 
 </html>
