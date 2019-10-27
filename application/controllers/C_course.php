b93d25735b9521177f9a2c5b881d8e6b9452cd36
francobko <franco.condor.urp@gmail.com>
2019-10-27 08:41:50 +0300

    Course

---
+++ application/controllers/C_course.php
@@ -0,0 +1,40 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class C_course extends CI_Controller 
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('utils');
+    }
+
+    public function index()
+    {
+        $sesionU = $this->session->userdata('s_nombreUsuario');
+        
+        //if (!empty($sesionU)) {
+            $data = array(
+            );
+            $this->load->view('V_home', $data);
+        //} else {
+        //    redirect('login');
+        //}
+        
+    }
+
+    public function cerrarSesion() {
+
+        $this->session->unset_userdata('s_idUsuario');
+        $this->session->unset_userdata('s_nombreUsuario');
+        $this->session->unset_userdata('s_nombrePersona');
+        $this->session->unset_userdata('s_roles');
+        $this->session->unset_userdata('s_area');
+        
+        if (!$this->session->userdata('s_idPersona_intranet')) {
+            $this->session->sess_destroy();
+        }
+        
+        redirect(base_url());
+    }
+}
---
+++ application/views/V_course.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="Ingresarport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <title>Mantenimiento - SB</title>
+
+    <link rel="stylesheet" href="<?php echo base_url(); ?>public/utils/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>public/utils/css/style.css">
+    <!-- Scrollbar Custom CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+
+</head>
+
+<body>
+    <div class="container">
+        <div class="content">
+            <p style="font-size:80px;font-weight:900;font-family: 'Montserrat Black', sans-serif">Pet learning</p>
+            <p style="font-size:35px;">Learn and study to take care of your virtual pet</p>
+            <button class="btn" onclick="goToLogin()">Start</button>
+        </div>
+        <img src="<?php echo base_url(); ?>public/utils/img/medusin.png" alt="logo" class="image-brand">
+    </div>
+    <script src="<?php echo base_url(); ?>public/utils/js/jquery-3.3.1.min.js"></script>
+    <script src="<?php echo base_url(); ?>public/utils/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.concat.min.js"></script>
+    <script>
+        function goToLogin() {
+
+            window.open("<?php echo base_url(); ?>" + "login", "_self");
+        }
+    </script>
+
+</body>
+
+</html>
